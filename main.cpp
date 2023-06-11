e77dea19a3a200f8736a4442830be0a3166ccf39
private94kb <91588734+private94kb@users.noreply.github.com>
2023-06-12 00:14:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+  int vector[]= {7, -8, 9, 6, -11, 8, 3, 9, -1 };
+  int n = sizeof(vector) / sizeof(vector[0]);
+  int max=(vector[0]);
 
+  for(int i = 0; i<n; i++){
+    if(*(vector+i)>max){
+      max = *(vector+i);        
+      }
+    }
+  cout<<max;
+
+  return 0;
+}
